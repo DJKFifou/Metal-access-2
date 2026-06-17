dea2bc676d9d3dd7288ac1d0030e12992a76f7e8
Maxime Lust <maxime@favoreatdesign.com>
2026-06-17 12:26:33 +0200

    update header

---
+++ src/lib/components/header.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import oeilBlanc from '$lib/assets/oeil-blanc.svg';
+	import plus from '$lib/assets/plus.svg';
+	// import arrowTopRight from '$lib/assets/arrow-top-right.svg';
+</script>
+
+<header class="fixed w-full">
+	<div class=" flex items-center justify-between px-16 py-6 font-fledora uppercase">
+		<div class="flex items-center gap-4 p-2 bg-theme-orange">
+			<img src={oeilBlanc} alt="Logo" class="inset-0 w-11.5 h-11.5 min-h-0 object-contain" />
+			<span class="uppercase text-theme-white bg-theme-orange text-3xl leading-[80%] pt-1"
+				>Metal <br /> access</span
+			>
+		</div>
+		<nav class="flex gap-6 items-center text-theme-white">
+			<div>
+				<span class="flex items-center gap-2 leading-[80%]"
+					>Bonnes pratiques
+					<img src={plus} alt="plus" class="w-5 h-full object-contain" />
+				</span>
+				<div></div>
+			</div>
+			<div>
+				<span class="flex items-center gap-2 leading-[80%]"
+					>La fédération
+					<img src={plus} alt="plus" class="w-5 h-full object-contain" />
+				</span>
+				<div></div>
+			</div>
+			<a href="/handicaps" class="leading-[80%]">Les handicaps</a>
+			<a href="/ressources" class="leading-[80%]">Ressources</a>
+		</nav>
+		<div class="flex gap-4 items-center">
+			<a
+				href=""
+				target="_blank"
+				class="flex gap-2 p-4 border border-theme-white text-theme-white items-center uppercase leading-[80%]"
+			>
+				Faire mon audit
+				<!-- <img src={arrowTopRight} alt="arrow top right" class="w-3.75 h-full object-contain" /> -->
+			</a>
+			<a href="/mon-festival" class="bg-theme-orange text-theme-black leading-[80%] p-4"
+				>Évaluer mon festival</a
+			>
+		</div>
+	</div>
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,34 +1,12 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/oeil.svg';
-	import oeilBlanc from '$lib/assets/oeil-blanc.svg';
-	import menu from '$lib/assets/menu.svg';
+	import Header from '$lib/components/header.svelte';
 
-	let { data, children } = $props();
+	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
-<header class="fixed w-full">
-	<div class=" flex items-center px-16 py-6 font-fledora text-white">
-		<div class="flex-1">
-			<img
-				src={oeilBlanc}
-				alt="Logo"
-				class="inset-0 w-15 h-15 min-h-0 p-2 bg-theme-orange object-contain"
-			/>
-		</div>
-		<div class="flex-1 flex justify-center">
-			<span class="uppercase pb-2 pt-3 px-8 bg-theme-orange text-center text-3xl leading-[80%]"
-				>Metal <br /> access</span
-			>
-		</div>
-		<div class="flex-1 flex justify-end items-center gap-4">
-			<span class="uppercase text-xl leading-[80%]">Menu</span>
-			<button class="cursor-pointer p-4 border border-theme-lightOrange bg-theme-orange">
-				<img src={menu} alt="Menu" class="w-4.5 h-full object-contain" />
-			</button>
-		</div>
-	</div>
-</header>
+<Header />
 {@render children()}
