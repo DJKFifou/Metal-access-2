772ceeb39f8ddc00c8a3618652ea0eb3c8ce018d
Maxime Lust <maxime@favoreatdesign.com>
2026-06-17 10:55:30 +0200

    move hero.png in static

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <section
 	class="h-lvh w-full flex items-end bg-cover bg-center px-18.75 py-20"
-	style="background-image: linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.3)), url('/src/lib/hero.png');"
+	style="background-image: linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.3)), url('/hero.png');"
 >
 	<h1 class="relative font-fledora text-theme-white text-6.5xl! max-w-2/3">
 		Lorem ipsum dolor sit amet consectetur.
